4612ad67f689ecf6989396e9dc4c82e719c5bb95
KarthikeyanSivanesan23 <karthikeyansivanesan23@gmail.com>
2026-08-31 10:48:40 +0530

    Add touch support for mobile parallax

--- src/components/Invitation.jsx
+++ src/components/Invitation.jsx
@@ -4,24 +4,37 @@ export default function Invitation() {
   useEffect(() => {
     const card = document.getElementById('invitationCard')
     if (!card) return
-    const handleMouseMove = (e) => {
+
+    const handleMove = (clientX, clientY) => {
       const rect = card.getBoundingClientRect()
-      const x = e.clientX - rect.left
-      const y = e.clientY - rect.top
+      const x = clientX - rect.left
+      const y = clientY - rect.top
       const centerX = rect.width / 2
       const centerY = rect.height / 2
       const rotateX = ((y - centerY) / centerY) * -10
       const rotateY = ((x - centerX) / centerX) * 10
       card.style.transform = `perspective(1000px) rotateX(${rotateX}deg) rotateY(${rotateY}deg) scale3d(1.02,1.02,1.02)`
     }
-    const handleMouseLeave = () => {
+
+    const handleMouseMove = (e) => handleMove(e.clientX, e.clientY)
+    const handleTouchMove = (e) => {
+      e.preventDefault()
+      handleMove(e.touches[0].clientX, e.touches[0].clientY)
+    }
+    const handleEnd = () => {
       card.style.transform = 'perspective(1000px) rotateX(0deg) rotateY(0deg) scale3d(1,1,1)'
     }
+
     card.addEventListener('mousemove', handleMouseMove)
-    card.addEventListener('mouseleave', handleMouseLeave)
+    card.addEventListener('touchmove', handleTouchMove, { passive: false })
+    card.addEventListener('mouseleave', handleEnd)
+    card.addEventListener('touchend', handleEnd)
+
     return () => {
       card.removeEventListener('mousemove', handleMouseMove)
-      card.removeEventListener('mouseleave', handleMouseLeave)
+      card.removeEventListener('touchmove', handleTouchMove)
+      card.removeEventListener('mouseleave', handleEnd)
+      card.removeEventListener('touchend', handleEnd)
     }
   }, [])
 
